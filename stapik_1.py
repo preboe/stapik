3f26cf7b876b1ebd37aa5310129516ee6601fcd0
arkadie <preboe25@gmail.com>
2020-12-19 14:32:53 +0700

    Первое занияе

---
+++ stapik_1.py
@@ -0,0 +1,42 @@
+# print(2 ** 0)
+# print(2 ** 1)
+# print(2 ** 2)
+# print(2 ** 3)
+# print(2 ** (-1))
+#
+#
+#
+# print(10 // 3)
+# print(10 // 4)
+# print(10 // 5)
+# print(10 // 6)
+# print(10 // 12)
+
+
+# print(10 % 3)
+# print(10 % 4)
+# print(10 % 5)
+# print(10 % 6)
+# print(10 % 12)
+# print(10 % 20)
+
+
+# stepik 2.5
+
+# b1 = int(input())
+# q = int(input())
+# n = int(input())
+# print(b1 * q ** (n - 1))
+
+
+# a = int(input())
+# print(a // 100)
+#
+# k = int(input())
+# n = int(input())
+# print(n // k)
+# print(n % k)
+
+n = int(input())
+print(n ** (100 // -50))
+
