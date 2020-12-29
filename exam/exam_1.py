142c88a05b96941f5b21c4b2c3fc952d8ceefa5b
arkadie <preboe25@gmail.com>
2020-12-29 13:27:42 +0700

    Добавлена папка exam.

---
+++ exam/exam_1.py
@@ -0,0 +1,52 @@
+# print('1', '2', '3', '4', sep='*')
+
+
+# s = 13
+# k = -5
+# d = s + 2
+# s = d
+# k = 2 * s
+# print(s + k + d)
+
+
+# count = int(input())
+# print("* " * count)
+# for _ in range(count - 2):
+#     print("* ", "  " * (count - 3), "* ")
+# print("* " * count)
+
+# w = 17
+# h = 4
+#
+# for i in range(h):
+#     if not i or i == h-1:
+#         print('*'*w, end ='')
+#         print()
+#     else:
+#          print('*' + ' '*(w-2) + '*')
+
+
+
+
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# d = int(input())
+# print(a ** b + c ** d)
+
+
+# a = int(input())
+# aa = 10*a + a
+# aaa = 100*a + aa
+# print(a + aa + aaa)
+
+
+# a = int(input())
+# b = int(input())
+# n1 = (a + b) ** 2
+# n2 = a ** 2 + b ** 2
+# print('Квадрат суммы', a, 'и', b, 'равен', n1)
+# print('Сумма квадратов', a, 'и', b, 'равна', n2)
+
+
+
